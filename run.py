409d953058bc3f5997b639f1df9d067a15a513f1
Vincent Chinedu Okonkwo <chinedu@gmail.com>
2014-11-03 15:21:52 +0530

    enable runnable migration on data model

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from ebola import app
 
-
-
-
 if __name__ == '__main__':
-	app.run(debug=True)
+
+    from flask_migrate import MigrateCommand
+    from flask_script import Manager
+
+    manager = Manager(app)
+    manager.add_command("schema", MigrateCommand)
+    manager.run( default_command="runserver")
