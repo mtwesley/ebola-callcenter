d4d5dce310f304ae445aa0aeff002f00cd02ddf6
Vincent Chinedu Okonkwo <chinedu@gmail.com>
2014-11-05 16:51:20 +0530

    expanded startup options

--- run.py
+++ run.py
@@ -1,10 +1,31 @@
-from ebola import app
+
+from flask_script import Manager
+from flask_script.commands import Command, ShowUrls, Option
+
+
+class DBCreator(Command):
+    def __init__(self):
+        Command.__init__(self)
+        self.add_option(Option('-d', '--drop',default=False, dest='drop'))
+
+    def handle(self, f_app, *args, **kwargs):
+        if kwargs.get("drop"):
+            app_db.drop_all(app)
+
+        app_db.create_all()
+
 
 if __name__ == '__main__':
 
     from flask_migrate import MigrateCommand
-    from flask_script import Manager
+
+    from os import environ
+    environ.setdefault('EBC_CONFIG', "ebola.config.ConfigDev")
+
+    from ebola import app, app_db
 
     manager = Manager(app)
     manager.add_command("schema", MigrateCommand)
-    manager.run( default_command="runserver")
+    manager.add_command("create_db", DBCreator )
+    manager.add_command("urls", ShowUrls)
+    manager.run( default_command="runserver" )
