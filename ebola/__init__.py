a75e10ba7f9decdf1f1b47e2ac4c23cf9cb3d9dd
Vincent Chinedu Okonkwo <chinedu@gmail.com>
2014-11-05 16:51:00 +0530

    model startup and cleanup

--- ebola/__init__.py
+++ ebola/__init__.py
@@ -1,5 +1,34 @@
-from flask import Flask
 
-app = Flask(__package__)
+from datetime import date
 
-from . import db
+from gevent import monkey
+monkey.patch_all()
+
+from gevent_psycopg2 import monkey_patch
+monkey_patch()
+
+from flask.app import Flask, g
+from flask_migrate import Migrate
+
+from .db import app_db
+from .api import api
+from .ui import ui
+
+app = Flask(__package__, static_folder="../static/", static_path="/static")
+app.config.from_envvar('EBC_CONFIG', True)
+
+app_db.init_app(app)
+Migrate(app, app_db)
+
+app.register_blueprint(api)
+app.register_blueprint(ui)
+
+@app.before_request
+def on_new_request():
+    g.timestamp = date.today()
+
+
+
+@app.route("/logout", endpoint="logout")
+def logout_user():
+    pass
